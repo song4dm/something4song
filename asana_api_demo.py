dd5566dd1dd1f1d5a7d994292451e433395160bb
song4dm <wanyu.song@paypay-corp.co.jp>
2023-06-02 00:02:09 +0900

    add get inquiry task detail method

--- asana_api_demo.py
+++ asana_api_demo.py
@@ -82,9 +82,9 @@ def out_put_to_csv(data, file_name):
 
 def get_backlog_tasks(project_gid, base_url, headers):
     print("get_backlog_tasks")
-    tasks = get_sections_in_project(project_gid, base_url, headers)
+    sections = get_sections_in_project(project_gid, base_url, headers)
     backlog_gid = ""
-    for task in tasks["data"]:
+    for task in sections["data"]:
         if task["name"] == "Backlog":
             print(task["gid"])
             backlog_gid = task["gid"]
@@ -101,10 +101,65 @@ def get_backlog_tasks(project_gid, base_url, headers):
             print(task_info["data"]["assignee"])
             if task_info["data"]["assignee"] != None:
                 task_infos.append({"gid":gid, "task_name":task_info["data"]["name"],"assignee":task_info["data"]["assignee"]["name"]})
-        print(task_infos)    
+        # print(task_infos)    
     else:
         print("Backlog section not exists.")
     
     
             
-get_backlog_tasks(project_gid, base_url, headers)
+# get_backlog_tasks(project_gid, base_url, headers)
+
+def get_inquiry_tasks(project_gid, base_url, headers):
+    print("get_inquiry_tasks")
+    sections = get_sections_in_project(project_gid, base_url, headers)
+    dme_sections_tag_list = ["Pending(every Friday check)", "Archive", "Done(Weekly)", "Done(Weekly)", "In Review", "In Progress", "Todo(This week)", "Backlog"]
+    section_gid_list = []
+    # get specified section gid
+    for task in sections["data"]:
+        if task["name"] in dme_sections_tag_list:
+            section_gid = task["gid"]
+            section_gid_list.append(section_gid)
+            break
+    print(section_gid_list)
+    
+    # get every section's task gid
+    section_task_gid_list = []
+    for section_gid in section_gid_list:
+        section_tasks = get_tasks_from_section(section_gid, base_url, headers)
+        for task in section_tasks["data"]:
+            section_task_gid_list.append(task["gid"])    
+    print(section_task_gid_list)
+    
+    task_detail_list = []
+    # dme_inquiry_tag_list = ["問い合わせ(Jira)", "問い合わせ(Slack)"]
+    for task_gid in section_task_gid_list:
+        task_detail = get_one_task(task_gid, base_url, headers)
+        for field in task_detail["data"]["custom_fields"]:
+            if field["display_value"] is not None and "問い合わせ" in field["display_value"]:
+                # print(field["display_value"])
+                task_detail_list.append(task_detail)
+    print(task_detail_list)
+        
+    # section_task_gid = ""
+    # for task in sections["data"]:
+    #     if task["name"] == "Backlog":
+    #         print(task["gid"])
+    #         section_task_gid = task["gid"]
+    
+    # if section_task_gid != "":
+    #     backlog_tasks = get_tasks_from_section(section_task_gid, base_url, headers)
+    #     task_gids = []
+    #     for task in backlog_tasks["data"]:
+    #        task_gids.append(task["gid"]) 
+    #     # print(task_gids)  
+    #     task_infos = [] 
+    #     for gid in task_gids:
+    #         task_info = get_one_task(gid, base_url, headers)
+    #         print(task_info["data"]["assignee"])
+    #         if task_info["data"]["assignee"] != None:
+    #             task_infos.append({"gid":gid, "task_name":task_info["data"]["name"],"assignee":task_info["data"]["assignee"]["name"]})
+    #     # print(task_infos)    
+    # else:
+    #     print("Backlog section not exists.")
+    
+get_inquiry_tasks(project_gid, base_url, headers)
